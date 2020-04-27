2fbdca314a1155121006016fad5fadbc3bb9d878
Payam <payambapiri.97@gmail.com>
2020-04-28 04:12:46 +0430

    payam.c: Improve readability

--- payam.c
+++ payam.c
@@ -8,46 +8,26 @@
 #define COLOR_LIGHT_FN ".color-light"
 #define COLOR_DARK_FN  ".color-dark"
 
-char **get_colorname (char **def)
-{
+char **get_colorname (char **def) {
   time_t t = time(NULL);
   struct tm *T = localtime(&t);
   char *home = getenv("HOME");
-  int path_len;
-  char *path;
-  FILE *f;
+  char path[2048];  
   static char c[COLOR_NUM][COLOR_LEN];
-  int i;
-
-  path_len = sizeof(char) * strlen(home) + 1 + strlen(COLOR_LIGHT_FN) + 1;
-  path = (char *) malloc(path_len);
   
-  if(path == NULL) return def;
-
-  for(i = 0; i < path_len; i++)
-    path[i] = '\0';
-  strcat(path, home);
-  strcat(path, "/");
+  if(T->tm_hour > 6 && T->tm_hour <= 22)
+    sprintf(path, "%s/%s", home, COLOR_LIGHT_FN);
+  else sprintf(path, "%s/%s", home, COLOR_DARK_FN);
   
-  if( T->tm_hour > 6 && T->tm_hour <= 22 )
-    strcat(path, COLOR_LIGHT_FN);
-  else
-    strcat(path, COLOR_DARK_FN);
-
-  f = fopen(path, "r");
-  free(path);
-        
-  for(i = 0; i < COLOR_NUM; i++)
-    {
-      fgets(c[i], COLOR_LEN, f);
-      c[i][COLOR_LEN-2] = '\0';
-    }
-
+  FILE *f = fopen(path, "r");
+  for(int i = 0; i < COLOR_NUM; i++) {
+    fgets(c[i], COLOR_LEN, f);
+    c[i][COLOR_LEN-2] = '\0';
+  }
   fclose(f);
   
-  def[0] = c[0]; // bg
-  def[7] = c[1]; // fg
+  def[0] = c[0]; /* Background */
+  def[7] = c[1]; /* Foreground */
   
   return def;
 }
-
