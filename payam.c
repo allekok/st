4e407643689b9a7929ef7b3ada1f1d4429052e0a
Payam <payambapiri.97@gmail.com>
2020-05-08 14:27:42 +0430

    payam.c: Use environment variable 'COLORNOW'

--- payam.c
+++ payam.c
@@ -6,16 +6,13 @@
 #define COLOR_LEN 9
 
 char **get_colorname (char **def) {
-	char buf[2048], path[2048];
+	char path[1024];
 	static char c[COLOR_NUM][COLOR_LEN];
-
-	sprintf(path, "%s/.color-now", getenv("HOME"));
+	
+	sprintf(path, "%s/.color-%s",
+		getenv("HOME"),
+		getenv("COLORNOW"));
 	FILE *f = fopen(path, "r");
-	fgets(buf, sizeof(buf), f);
-	fclose(f);
-  
-	sprintf(path, "%s/.color-%s", getenv("HOME"), buf);
-	f = fopen(path, "r");
 	for(int i = 0; i < COLOR_NUM; i++) {
 		fgets(c[i], COLOR_LEN, f);
 		c[i][COLOR_LEN-2] = '\0';
